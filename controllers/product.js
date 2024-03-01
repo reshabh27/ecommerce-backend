4b2b7850c53dc7fbd293260f1eb72d12e5bcef31
reshabh27 <reshabhsahu10@gmail.com>
2024-03-01 11:24:55 +0530

    Added asyncErrorHandler to catch async await errors

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,11 @@
 const Product = require("../models/products");
+const CustomError = require("../utils/CustomError");
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
 
 // GET /tasks?limit=1&skip=2
 // GET /tasks?sortBy=createdAt:desc
-exports.handleGetAllProducts = async (req, res) => {
+exports.handleGetAllProducts = asyncErrorHandler(async (req, res, next) => {
     // const match = {};
     const sort = {};
     let limit = parseInt(req.query.limit) || 0;
@@ -14,67 +16,52 @@ exports.handleGetAllProducts = async (req, res) => {
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
-    try {
-        const products = await Product.find().sort(sort).skip(skip).limit(limit);
+    const products = await Product.find().sort(sort).skip(skip).limit(limit);
 
-        res.status(200).send(products);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-};
+    res.status(200).send(products);
+});
 
-exports.handleGetProductById = async (req, res) => {
-    try {
-        // console.log(req.params);
-        const product = await Product.findById(req.params.id)
-        return res.send(product);
-    } catch (err) {
-        return res.send(err);
-    }
-}
+exports.handleGetProductById = asyncErrorHandler(async (req, res, next) => {
+    // console.log(req.params);
+    const product = await Product.findById(req.params.id)
+    // console.log(product);
+    if (!product)
+        return next(new CustomError(`Product is not available`, 404));
+    return res.send(product);
+})
 
 
-exports.handleAddProduct = async (req, res) => {
+exports.handleAddProduct = asyncErrorHandler(async (req, res, next) => {
     if (req.role === 'user') {
         return res.status(403).send({ message: "You do not have permission to perform this action." });
     }
-    try {
-        const product = new Product(req.body);
-        // console.log(product);
-        await product.save();
-        return res.status(201).send({ message: "Successfully added product." })
-    } catch (err) {
-        console.log(err);
-        return res.send(err);
-    }
-}
+    const product = new Product(req.body);
+    // console.log(product);
+    await product.save();
+    return res.status(201).send({ message: "Successfully added product." })
+})
 
-exports.handleProductUpdate = async (req, res) => {
+exports.handleProductUpdate = asyncErrorHandler(async (req, res, next) => {
     if (req.role === 'user') {
         return res.status(403).send({ message: "You do not have permission to perform this action." });
     }
-    try {
-        const updates = req.body;
-        // console.log(updates);
-        await Product.findByIdAndUpdate(req.params.id, updates);
-        return res.send("updated fields successfully");
-    } catch (err) {
-        return res.send(err);
-    }
-}
+    const updates = req.body;
+    // console.log(updates);
+    await Product.findByIdAndUpdate(req.params.id, updates);
+    return res.send("updated fields successfully");
+})
 
 
 
-exports.handleProductDelete = async (req, res) => {
+exports.handleProductDelete = asyncErrorHandler(async (req, res, next) => {
     if (req.role === 'user') {
-        return res.status(403).send({ message: "You do not have permission to perform this action." });
+        return next(new CustomError(`You do not have permission to perform this action.`, 403));
     }
-    try {
-        await Product.findByIdAndDelete(req.params.id);
-        res.send("product deleted successfully");
-    } catch (err) {
-        return res.send(err);
-    }
-}
+    const isAvailable = await Product.findById(req.params.id);
+    if (!isAvailable)
+        return next(new CustomError(`Product already Deleted`, 404));
+    await Product.findByIdAndDelete(req.params.id);
+    res.send("product deleted successfully");
+})
 
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,60 +1,43 @@
 const User = require("../models/user")
+const asyncErrorHandler = require("../utils/asyncErrorHandler")
 
 
 
-exports.handleSignUp = async (req, res) => {
+exports.handleSignUp = asyncErrorHandler(async (req, res, next) => {
     const user = new User(req.body)
-
-    try {
-        await user.save()
-        // console.log(user);
-        // sendWelcomeEmail(user.email, user.name)
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-}
+    await user.save()
+    // console.log(user);
+    // sendWelcomeEmail(user.email, user.name)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
+})
 
 
 
-exports.handleLogIn = async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        // console.log(user);
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (e) {
-        res.status(400).send()
-    }
-}
+exports.handleLogIn = asyncErrorHandler(async (req, res, next) => {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    // console.log(user);
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+})
 
 
 
-exports.handleLogOut = async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
-
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-}
+exports.handleLogOut = asyncErrorHandler(async (req, res, next) => {
+    req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token !== req.token
+    })
+    await req.user.save()
+    res.send()
+})
 
 
 
-exports.handleLogOutAll = async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send({ message: `${req.user.email} logged out from all session` })
-    } catch (e) {
-        res.status(500).send()
-    }
-}
+exports.handleLogOutAll = asyncErrorHandler(async (req, res, next) => {
+    req.user.tokens = []
+    await req.user.save()
+    res.send({ message: `${req.user.email} logged out from all session` })
+})
 
 
 exports.handleGetMyProfile = async (req, res) => {
@@ -62,7 +45,7 @@ exports.handleGetMyProfile = async (req, res) => {
 }
 
 
-exports.handleUpdateMyProfile = async (req, res) => {
+exports.handleUpdateMyProfile = asyncErrorHandler(async (req, res, next) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -71,26 +54,18 @@ exports.handleUpdateMyProfile = async (req, res) => {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
 
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-}
+    updates.forEach((update) => req.user[update] = req.body[update])
+    await req.user.save()
+    res.send(req.user)
+})
 
-exports.handleDeleteMyProfile = async (req, res) => {
-    try {
-        // console.log(req.user._id);
-        await User.findByIdAndDelete(req.user._id)
-        // await req.user.remove()
-        // sendCancelationEmail(req.user.email, req.user.name)
-        res.send(req.user)
-    } catch (e) {
-        res.status(500).send()
-    }
-}
+exports.handleDeleteMyProfile = asyncErrorHandler(async (req, res, next) => {
+    // console.log(req.user._id);
+    await User.findByIdAndDelete(req.user._id)
+    // await req.user.remove()
+    // sendCancelationEmail(req.user.email, req.user.name)
+    res.send(req.user);
+})
 
 
 
