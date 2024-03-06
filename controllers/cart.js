87be32a68d8d1d2f4f7417aa5e4ca3f60b578938
reshabh27 <reshabhsahu10@gmail.com>
2024-03-06 15:16:37 +0530

    Implemented Delete product in Cart functionality

--- controllers/cart.js
+++ controllers/cart.js
@@ -22,6 +22,7 @@ exports.handleAddProductInCart = asyncErrorHandler(async (req, res, next) => {
     if (oldEntry) {
         oldEntry.quantity = req.body.quantity,
             await oldEntry.save();
+        return res.status(202).send({ message: "Product Updated in Cart" });
     }
     else {
         const cart = new Cart({
@@ -30,19 +31,19 @@ exports.handleAddProductInCart = asyncErrorHandler(async (req, res, next) => {
             quantity: req.body.quantity
         });
         await cart.save();
+        return res.send("succesfully added product in cart");
     }
-    return res.send("succesfully added product in cart");
 })
 
 exports.handleDeleteProductInCart = asyncErrorHandler(async (req, res, next) => {
     const productId = req.params.productId;
     // console.log(productId);
-    const oldCart = await Cart.findOne({ owner: req.user._id });
+    const oldCart = await Cart.findOne({ userId: req.user._id, productId });
+    // console.log(oldCart);
     if (!oldCart) {
-        return res.status(404).send({ error: 'Cart not found' });
+        return res.status(404).send({ error: 'Product Already deleted' });
     }
-
     // Use $pull to remove the specified productId from the products array
-    await Cart.findByIdAndUpdate(oldCart._id, { $pull: { products: productId } });
+    await Cart.findByIdAndDelete(oldCart._id);
     res.send(`successfully deleted product ${productId}`);
 })
